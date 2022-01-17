84ebdd37b5ba12f22813eace694072c172427580
Gabriel_Cara <72806585+Gabriel-Cara@users.noreply.github.com>
2022-01-17 15:16:53 -0300

    Files atualizado

--- aboutUs/mobile-navbar.js
+++ aboutUs/mobile-navbar.js
@@ -1,5 +1,5 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks){
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
@@ -8,15 +8,15 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    animateLinks(){
-        this.navLinks.forEach((link) => {    
+    animateLinks() {
+        this.navLinks.forEach((link) => {
             link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
         });
     }
 
-    handleClick(){
+    handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
@@ -26,8 +26,8 @@ class MobileNavbar {
         this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
-    init(){
-        if (this.mobileMenu){
+    init() {
+        if (this.mobileMenu) {
             this.addClickEvent();
         }
         return this;
@@ -35,10 +35,10 @@ class MobileNavbar {
 }
 
 const mobileNavbar = new MobileNavbar(
-".mobile-menu",
-".nav-list",
-".nav-list li",
-".nav-menu",
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+    ".nav-menu",
 );
 
 mobileNavbar.init();
